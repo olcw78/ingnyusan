2a4885b2e31d2bcc1191cad69b9a0f8a986b1a73
yoonsang lee <highp0912@pm.me>
2023-04-30 17:51:48 +0900

    add: Searchbar draft

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,26 +1,35 @@
+import SearchBar from "@/components/search-bar/SearchBar";
 import type { FC } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar: FC = () => (
   <div className="px-20 shadow-md navbar min-h-[25px]">
     <div className="flex-1">
-      <Link to="/" className="btn btn-ghost">
-        <span className="text-3xl">Ingnyusan</span>
-      </Link>
+      <button className="btn btn-ghost">
+        <Link to="/">
+          <span className="text-3xl">Ingnyusan</span>
+        </Link>
+      </button>
     </div>
+
+    <SearchBar />
 
     <div className="flex-none">
       <ul className="gap-x-2 px-1 menu menu-horizontal">
         <li>
-          <Link to="/Resume">
-            <p className="text-slate-400 text-md">Résumé</p>
-          </Link>
+          <button className="btn btn-ghost">
+            <Link to="/Resume">
+              <p className="text-slate-400 text-md">Résumé</p>
+            </Link>
+          </button>
         </li>
 
         <li>
-          <Link to="/Develop">
-            <p className="text-slate-400 text-md">Develop</p>
-          </Link>
+          <button className="btn btn-ghost">
+            <Link to="/Develop">
+              <p className="text-slate-400 text-md">Develop</p>
+            </Link>
+          </button>
         </li>
       </ul>
     </div>
---
+++ src/components/search-bar/SearchBar.tsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Search Anything!.."
+      className="input input-bordered w-full max-w-xs"
+    />
+  );
+};
+
+export default SearchBar;
