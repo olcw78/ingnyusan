17a88508668db837f6f09ccf1416f90dec089034
yoonsang lee <highp0912@pm.me>
2023-05-12 19:42:11 +0900

    add: website title / desc 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ingnyusan';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ingnyusan | 잉뉴산';
+export const SITE_DESCRIPTION = 'blog of Front End Developer Yoonsang Lee.';
