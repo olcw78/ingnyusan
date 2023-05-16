9aac72090603dc61b9ab976de4329fa1caad3fca
yoonsang lee <highp0912@pm.me>
2023-05-16 23:29:03 +0900

    svg import

--- src/components/ThemeIcon.tsx
+++ src/components/ThemeIcon.tsx
@@ -1,29 +1,27 @@
-import type { FC } from "preact/compat";
-import { useStore } from "@nanostores/preact";
+import type {FC} from "preact/compat";
+import {useStore} from "@nanostores/preact";
 import {
-  preferences,
-  toggleTheme,
-  loadInitialThemePreferences,
+    preferences,
+    toggleTheme,
+    loadInitialThemePreferences,
 } from "../states/Theme.state";
-import SunIcon from "../assets/svg/Sun.svg";
-import MoonIcon from "../assets/svg/Moon.svg";
-import { useEffect } from "preact/hooks";
+import {useEffect} from "preact/hooks";
 
 const ThemeIcon: FC = () => {
-  const { theme } = useStore(preferences);
+    const {theme} = useStore(preferences);
 
-  useEffect(() => {
-    loadInitialThemePreferences();
-  }, []);
+    useEffect(() => {
+        loadInitialThemePreferences();
+    }, []);
 
-  return (
-    <button class="border-0 bg-transparent btn" onClick={toggleTheme}>
-      {theme === "light" ? (
-        <img src={SunIcon} alt="sun icon" />
-      ) : (
-        <img src={MoonIcon} alt="moon icon" />
-      )}
-    </button>
-  );
+    return (
+        <button class="border-0 bg-transparent btn" onClick={toggleTheme}>
+            {theme === "light" ? (
+                <img src="../assets/svg/sun.svg" alt="sun icon"/>
+            ) : (
+                <img src="../assets/svg/moon.svg" alt="moon icon"/>
+            )}
+        </button>
+    );
 };
 export default ThemeIcon;
