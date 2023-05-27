3a4d0d2056e411ba987c58485ec6b932de6c3303
yoonsang lee <highp0912@pm.me>
2023-05-27 19:01:21 +0900

    add: image/optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,31 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import sanity from "astro-sanity";
+
+import image from "@astrojs/image";
+
+const sanityCfg = {
+  name: "default",
+  title: "ingnyusan-blog-cms",
+  projectId: "07nhbugi",
+  dataset: "production",
+  useCdn: true,
+};
+
+const imageCfg = {
+  serviceEntryPoint: "@astrojs/image/sharp",
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ingnyusan.com",
-  integrations: [mdx(), sitemap(), preact(), tailwind(), sanity({
-    name: "default",
-    title: "ingnyusan-blog-cms",
-    projectId: "07nhbugi",
-    dataset: "production",
-    useCdn: true
-  })]
+  integrations: [
+    mdx(),
+    sitemap(),
+    preact(),
+    tailwind(),
+    sanity(sanityCfg),
+    image(imageCfg),
+  ],
 });
