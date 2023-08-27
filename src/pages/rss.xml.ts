965b3841c6a3f6555c41f3c6e28854a1e03d193f
yoonsang lee <highp0912@pm.me>
2023-08-27 18:40:37 +0900

    fix: disable rss temporarily

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,30 +1,31 @@
-import rss from "@astrojs/rss";
-import { getAllBlogs } from "../cms/getAllBlogs";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
-import type { Writeable } from "astro/zod";
+// import rss from "@astrojs/rss";
+// import { getAllBlogs } from "../cms/getAllBlogs";
+// import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+// import type { Writeable } from "astro/zod";
 
-export async function get(context: any) {
-  const blogs = await getAllBlogs();
+// export async function get(context: any) {
+//   const blogs = await getAllBlogs();
 
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.url.href,
-    customData: "<language>ko-kr</language>",
+//   return rss({
+//     title: SITE_TITLE,
+//     description: SITE_DESCRIPTION,
+//     site: context.url.href,
+//     customData: "<language>ko-kr</language>",
 
-    items: blogs.map((blog) => ({
-      title: blog.title,
-      description: blog.brief,
-      pubDate: blog.publishedAt,
-      author: "yoonsang lee",
-      categories: blog.categories as Writeable<string[]>,
-      content:
-        "<p>" +
-        blog.content
-          .flatMap(({ children }) => children.map((e) => e.text))
-          .join("") +
-        "</p>",
-      link: `/blog/${blog.slug.current}/`,
-    })),
-  });
-}
+//     items: blogs.map((blog) => ({
+//       title: blog.title,
+//       description: blog.brief,
+//       pubDate: blog.publishedAt,
+//       author: "yoonsang lee",
+//       // customData: b
+//       // categories: blog.categories as Writeable<string[]>,
+//       // content:
+//       //   "<p>" +
+//       //   blog.content
+//       //     .flatMap(({ children }) => children.map((e) => e.text))
+//       //     .join("") +
+//       //   "</p>",
+//       link: `/blog/${blog.slug.current}/`,
+//     })),
+//   });
+// }
